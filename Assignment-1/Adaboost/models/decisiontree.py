4f70fefdd492e14840c76668442c12cf05794cbb
Subangkar <subangkar.karmaker@gmail.com>
2020-03-18 20:55:56 +0600

    decision tree: information gain modified
    kept 2 prediction for cut off node

--- Assignment-1/Adaboost/models/decisiontree.py
+++ Assignment-1/Adaboost/models/decisiontree.py
@@ -32,20 +32,33 @@ class DtUtils:
 		return -p0 * np.log2(p0) - p1 * np.log2(p1)
 
 	# y = {0,1}
+	"""
+	:returns (information_gain, entropy_left, entropy_right)
+	"""
 	@staticmethod
-	def information_gain(x_parent, y_parent, split):
-		y_child0 = y_parent[x_parent <= split]
-		y_child1 = y_parent[x_parent > split]
+	def information_gain(x_parent, y_parent, split, entropy_parent=None):
+		y_child_le = y_parent[x_parent <= split]
+		y_child_gt = y_parent[x_parent > split]
 		N = y_parent.shape[0]
-		N_0 = y_child0.shape[0]
-		if N_0 == 0 or N_0 == N:
+		N_le = y_child_le.shape[0]
+		if N_le == 0 or N_le == N:
 			return 0
-		p0 = float(N_0) / N
-		p1 = 1 - p0
-		return DtUtils.entropy(y_parent) - p0 * DtUtils.entropy(y_child0) - p1 * DtUtils.entropy(y_child1)
+		p_le = float(N_le) / N
+		p_gt = 1 - p_le
+
+		entropy_child_le = DtUtils.entropy(y_child_le)
+		entropy_child_gt = DtUtils.entropy(y_child_gt)
+
+		if entropy_parent is None:
+			entropy_parent = DtUtils.entropy(y_parent)
+
+		return entropy_parent - p_le * entropy_child_le - p_gt * entropy_child_gt
+
+	# info_gain = entropy_parent - p_le * entropy_child_le - p_gt * entropy_child_gt
+	# return info_gain, entropy_child_le, entropy_child_gt
 
 	@staticmethod
-	def find_split(X, y, column):
+	def find_split(X, y, column, entropy_parent=None):
 		# Binarization -------------------
 		sort_idx = np.argsort(X[:, column])
 		x_values = X[sort_idx, column]
@@ -55,7 +68,8 @@ class DtUtils:
 		# --------------------------------
 
 		max_ig, split_thresh = max(
-			list(map(lambda split_value: (DtUtils.information_gain(x_values, y_values, split_value), split_value),
+			list(map(lambda split_value: (
+				DtUtils.information_gain(x_values, y_values, split_value, entropy_parent=entropy_parent), split_value),
 			         split_values)))
 		return max_ig, split_thresh
 
@@ -84,23 +98,35 @@ class TreeNode:
 	def make_terminal_node(self, X, y):
 		self.feature = None
 		self.split_threshold = None
-		self.left = None
-		self.right = None
+		self.left = self.right = None
 		self.prediction = collections.Counter(y).most_common()[0][0]
+
+	def make_cutoff_node(self, X, y, feature, split_thresh):
+		self.feature = feature
+		self.split_threshold = split_thresh
+		self.left = self.right = None
+
+		self.prediction = (collections.Counter(y[X[:, feature] <= split_thresh]).most_common()[0][0],
+		                   collections.Counter(y[X[:, feature] > split_thresh]).most_common()[0][0])
 
 	def buildtree(self, X, y, depth, max_depth=None):
 		# print(depth, end=' ')
 		self.entropy = DtUtils.entropy(y)
-		if len(y) == 1 or (max_depth is not None and depth >= max_depth) or self.entropy == 0:
+		if len(y) == 1 or self.entropy == 0:
 			self.make_terminal_node(X, y)
 			return
 
 		best_feature, max_ig, best_split_thresh = max(
-			list(map(lambda feature: ((feature,) + (DtUtils.find_split(X, y, feature))), range(X.shape[1]))),
+			list(map(lambda feature: ((feature,) + (DtUtils.find_split(X, y, feature, entropy_parent=self.entropy))),
+			         range(X.shape[1]))),
 			key=lambda v: v[1])
 
 		if max_ig <= 0:
 			self.make_terminal_node(X, y)
+			return
+
+		if max_depth is not None and depth >= max_depth:
+			self.make_cutoff_node(X, y, feature=best_feature, split_thresh=best_split_thresh)
 			return
 
 		self.feature = best_feature
@@ -119,9 +145,9 @@ class TreeNode:
 		if self.feature is None:
 			return self.prediction
 		if x[self.feature] <= self.split_threshold:
-			return self.left.predict_val(x)
+			return self.left.predict_val(x) if self.left is not None else self.prediction[0]
 		else:
-			return self.right.predict_val(x)
+			return self.right.predict_val(x) if self.right is not None else self.prediction[1]
 
 	# using loop
 	def predict(self, X):
